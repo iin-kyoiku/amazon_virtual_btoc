42d518349d7ef5e4672a4e1c74805a06277d8904
fuji <fuji>
2017-06-30 08:44:16 +0900

    新規作成

---
+++ src/main/java/jp/co/iinetwork/www/amazon_virtual_btoc/App.java
@@ -0,0 +1,13 @@
+package jp.co.iinetwork.www.amazon_virtual_btoc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
